3c001837da882259dd0e7d011d38af4492327b62
elisakaisa <elisa.k.perini@gmail.com>
2022-06-14 09:34:57 +0200

    added frontend

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+// Middleware
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 morgan.token("body", req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
